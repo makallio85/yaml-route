da44dfc0b5d6efd4bfd8198f20a41740c9b19bf4
Marko Kallio <marko.kallio@suomenpt.fi>
2016-01-07 21:35:04 +0200

    .

--- vendor/cakephp-plugins.php
+++
@@ -1,8 +0,0 @@
-<?php
-$baseDir = dirname(dirname(__FILE__));
-return [
-    'plugins' => [
-        'PluginAnimals' => $baseDir . '/tests/plugins/PluginAnimals/',
-        'PluginCars' => $baseDir . '/tests/plugins/PluginCars/',
-    ]
-];
