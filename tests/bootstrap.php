c0abe087f274ccbaff3112ce6f68400816a236de
Marko Kallio <marko.kallio@suomenpt.fi>
2016-01-08 10:15:00 +0200

    Autoload path correction.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 $loader = require dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DS . 'vendor' . DS . 'autoload.php';
-$loader->setPsr4("makallio85\\YamlRoute\\Test\\", dirname(dirname(dirname(dirname(dirname(__FILE__))))) . "./vendor/makallio85/yaml-route/tests/YamlRoute");
+$loader->setPsr4("makallio85\\YamlRoute\\Test\\", dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DS . 'vendor' . DS . 'makallio85' . DS . 'yaml-route' . DS . 'tests' . DS . 'YamlRoute');
 
 require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DS . 'config' . DS . 'bootstrap.php';
 
