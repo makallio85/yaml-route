d758703d7a01106b175f8c12e15a8d7c81daf84f
Marko Kallio <marko.kallio@suomenpt.fi>
2016-01-08 07:18:21 +0200

    Test updates

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DS . 'config' . DS . 'bootstrap.php';
+
+\Cake\Core\Configure::write(
+    ['plugins' => [
+        'PluginCars'    => dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DS . 'plugins' . DS . 'PluginCars',
+        'PluginAnimals' => dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DS . 'plugins' . DS . 'PluginAnimals',
+    ]]
+);
+
 makallio85\YamlRoute\Plugin::getInstance()->load('PluginCars', ['route' => true, 'bootstrap' => false]);
 makallio85\YamlRoute\Plugin::getInstance()->load('PluginAnimals', ['route' => true, 'bootstrap' => false]);
 
