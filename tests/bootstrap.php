f0cf310ade1cc90b9a69bcc7967b1d8b34a0b058
Marko Kallio <marko.kallio@suomenpt.fi>
2016-01-08 09:30:32 +0200

    Testing updates

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
 
+$loader = require dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DS . 'vendor' . DS . 'autoload.php';
+$loader->setPsr4("makallio85\\YamlRoute\\Test\\", dirname(dirname(dirname(dirname(dirname(__FILE__))))) . "./vendor/makallio85/yaml-route/tests/YamlRoute");
+
 require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))) . DS . 'config' . DS . 'bootstrap.php';
 
 \Cake\Core\Configure::write(
